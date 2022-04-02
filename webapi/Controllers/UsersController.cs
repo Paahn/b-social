e7956b4eb48c3779ec9cbc69689cdc6a9cddaa13
Panos <paahndev@gmail.com>
2022-04-01 17:21:40 -0700

    GetUsers method returns all users

--- webapi/Controllers/UsersController.cs
+++ webapi/Controllers/UsersController.cs
@@ -8,5 +8,18 @@ namespace WebApi.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
+        private readonly WebApiDbContext _context;
+        public UsersController(WebApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
     }
 }
