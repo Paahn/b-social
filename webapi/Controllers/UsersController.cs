aa6705a27d5183134c8c3a2a820a493430e80498
Panos <paahndev@gmail.com>
2022-04-01 17:21:21 -0700

    Create UsersController.cs

---
+++ webapi/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+using WebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+    }
+}
