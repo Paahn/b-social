475f8034c5059f85e135d3812444da2dcbdae38b
Panos <paahndev@gmail.com>
2022-05-31 22:45:17 -0700

    http call to backend to get users

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+export class AppComponent implements OnInit {
   public title: string = 'b-social';
+  public users: User;
+
   public constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
+      this.http.get('https://localhost:5001/api/users').subscribe();
   }
 }
