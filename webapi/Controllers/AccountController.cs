73945ebb59d0bc3df7f3e603ba3fa276c4397470
Panos <paahndev@gmail.com>
2023-01-26 19:16:35 -0800

    hash and salt user password

--- webapi/Controllers/AccountController.cs
+++ webapi/Controllers/AccountController.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Data;
 using WebApi.Models;
@@ -16,7 +18,15 @@ namespace WebApi.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(string username, string password)
         {
-            
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
         }
     }
 }
