c9440b952f0ad05e1c636d6ba92d3b8295335c0c
Panos <paahndev@gmail.com>
2023-03-14 09:23:32 -0700

    create token service

---
+++ webapi/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using WebApi.Interfaces;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
